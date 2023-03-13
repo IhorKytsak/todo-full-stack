7c613fd044fd436c0f4dda130e4be9e954fba7a5
AndrewAndreevix <andrejandrus962@gmail.com>
2023-03-13 09:56:22 +0200

    feat: Use postgres

--- packages/backend/src/config/database.ts
+++ packages/backend/src/config/database.ts
@@ -1,15 +1,36 @@
 /* eslint-disable no-console */
-import { ConnectionOptions, connect } from 'mongoose';
+
+import { createConnection, DataSourceOptions } from 'typeorm';
+
+function getSSLConfig(env: string) {
+  const configs: { [key: string]: boolean | { [key: string]: boolean } } = {
+    production: { rejectUnauthorized: true },
+    local: false,
+    deploy: { rejectUnauthorized: false }
+  };
+  if (!configs[env] === undefined) {
+    throw new Error('Set network in your .env file');
+  }
+  return configs[env];
+}
 
 const connectDB = async () => {
   try {
-    const options: ConnectionOptions = {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
+    const options: DataSourceOptions = {
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      logging: ['query', 'error'],
+      type: 'postgres',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      migrations: ['dist/migrations/**/*.{ts,js}'],
+      subscribers: ['src/subscriber/**/*.ts'],
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      ssl: getSSLConfig(process.env.SERVER_MODE),
+      synchronize: true
     };
-    await connect(process.env.MONGO_URI, options);
+    await createConnection(options);
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
