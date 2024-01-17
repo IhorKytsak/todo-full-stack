f84d0be440bc127ae33138cabe58897768dc7baf
Ihor Kytsak <kitsak735@gmail.com>
2024-01-17 14:49:18 +0200

    feat: Added interaction logic with DB and todo type

--- packages/backend/src/entities/Todo.entity.ts
+++ packages/backend/src/entities/Todo.entity.ts
@@ -3,7 +3,7 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class TodoEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
 
   @Column()
   title: string;
--- packages/backend/src/services/todo.service.ts
+++ packages/backend/src/services/todo.service.ts
@@ -1,5 +1,33 @@
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { TodoEntity } from '../entities/Todo.entity';
+import { ITodo } from '../types/todos.type';
+
 export default class TodoService {
-  async findAll() {
-    return 'Todos';
+  async findAll(): Promise<ITodo[]> {
+    const todos = await TodoEntity.find();
+    return todos;
+  }
+
+  async findOne(id: number): Promise<ITodo | null> {
+    const todo = await TodoEntity.findOneBy({ id });
+    return todo;
+  }
+
+  async addTodo(newTodo: ITodo): Promise<ITodo> {
+    const todoToCreate = TodoEntity.create({ ...newTodo });
+    await todoToCreate.save();
+    return todoToCreate;
+  }
+
+  async updateTodo(id: number, body: ITodo): Promise<UpdateResult> {
+    const updateResult = await TodoEntity.update({ id }, { ...body });
+
+    return updateResult;
+  }
+
+  async deleteTodo(todoId: number): Promise<DeleteResult> {
+    const deleteResult = await TodoEntity.delete(todoId);
+
+    return deleteResult;
   }
 }
