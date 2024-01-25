df78faf82cccf838bf4cd9a18fc7721220c585a3
Ihor Kytsak <kitsak735@gmail.com>
2024-01-25 12:01:52 +0200

    feat: Added filters for todo list by title and statuses on backend

--- packages/backend/src/types/todos.type.ts
+++ packages/backend/src/types/todos.type.ts
@@ -1,6 +1,21 @@
+import { FindOperator } from 'typeorm';
+
 export interface ITodo {
   title: string;
   description: string;
   isCompleted: boolean;
   isPrivate: boolean;
+}
+
+export interface ITodoQuery {
+  search?: string;
+  isCompleted?: 'true' | 'false';
+  isPrivate?: 'true' | 'false';
+}
+
+export interface ITodoFilter {
+  user: { id: number };
+  title?: FindOperator<string>;
+  isCompleted?: boolean;
+  isPrivate?: boolean;
 }
